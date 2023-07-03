3c48675ed9fd16b1308f59a0376d1e4ced9925ec
Mohamed Ali <somcoder@outlook.com>
2023-07-03 10:47:32 +0300

    Initial setup

---
+++ lua/somcoder/init.lua
@@ -0,0 +1,7 @@
+require('somcoder.packer')
+require('somcoder.remap')
+require('somcoder.set')
+
+require('lualine').setup {
+    options = { theme = 'onedark' }
+}
---
+++ lua/somcoder/packer.lua
@@ -0,0 +1,64 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({ 'rose-pine/neovim', as = 'rose-pine' })
+  use {'ray-x/starry.nvim', setup = function() 
+      -- see example setup below
+      vim.g.starry_italic_comments = true
+  end}
+
+  --vim.cmd('colorscheme rose-pine')
+
+  -- LuaLine: Bottom statusline
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  -- Treesitter
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+
+  use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v2.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},             -- Required
+		  {                                      -- Optional
+		  'williamboman/mason.nvim',
+		  run = function()
+			  pcall(vim.cmd, 'MasonUpdate')
+		  end,
+	  },
+	  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+	  -- Autocompletion
+	  {'hrsh7th/nvim-cmp'},     -- Required
+	  {'hrsh7th/cmp-nvim-lsp'}, -- Required
+	  {'L3MON4D3/LuaSnip'},     -- Required
+
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lua'},
+
+      -- Snippets
+      {'rafamadriz/friendly-snippets'},
+  }
+}
+
+end)
